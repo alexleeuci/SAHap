5deaf41d85d66c9bb2520b5f33992ab25ddc76a1
Alex Lee <alexlee0000000000@gmail.com>
2019-07-31 16:06:53 -0700

    added the votes matrix idea propsed, added fillVotes and destroyVotes functions, other minor bug fixes

--- test1.cpp
+++ test1.cpp
@@ -56,7 +56,7 @@ std::vector<std::string> readGenomeFile(std::string genomeFileName){
 	return out;
 }
 
-std::vector<std::string> readReadFile(std::string readFileName){
+std::vector<read> readReadFile(std::string readFileName){
 	std::ifstream readFile;
 	readFile.open(readFileName);
 	std::vector<read> out;
@@ -75,14 +75,49 @@ std::vector<std::string> readReadFile(std::string readFileName){
 	return out;
 }
 
-
-float MEC(std::vector<int> sol,  std::vector<std::string> genomes ,std::vector<read> reads){
+float MEC(std::vector<int> sol,  int k ,std::vector<read> reads, int*** votes){
 	float out;
 	//Complete this!
 	return out;
 }
 
-float score(std::vector<int> sol,  std::vector<std::string> genomes ,std::vector<read> reads){
+void fillVotes(std::vector<read> reads, std::vector<int> currentSolution, int*** votes, int& totalGenomeLen){
+	int max = 0;
+	for(int readIndex1 = 0; readIndex1<reads.size(); readIndex1++){
+		read readBeingAdded = reads[readIndex1];
+		for(int leftReadIndex = 0; leftReadIndex<readBeingAdded.leftRead.length(); leftReadIndex++){
+			char letterBeingAdded = readBeingAdded.leftRead;
+			int chromosomeIndex = currentSolution[readIndex1];
+			if(letterBeingAdded=='a'){votes[0][chromosomeIndex][readBeingAdded.pos+leftReadIndex]++;}
+			if(letterBeingAdded=='t'){votes[1][chromosomeIndex][readBeingAdded.pos+leftReadIndex]++;}
+			if(letterBeingAdded=='g'){votes[2][chromosomeIndex][readBeingAdded.pos+leftReadIndex]++;}
+			if(letterBeingAdded=='c'){votes[3][chromosomeIndex][readBeingAdded.pos+leftReadIndex]++;}
+		}
+		for(int rightReadIndex = 0; rightReadIndex<readBeingAdded.rightRead.length(); rightReadIndex++){
+			char letterBeingAdded = readBeingAdded.rightRead;
+			int rightmostIndex = readBeingAdded.pos2+rightReadIndex;
+			if(rightmostIndex<max){
+				max = rightmostIndex;
+			}
+			int chromosomeIndex = currentSolution[readIndex1];
+			if(letterBeingAdded=='a'){votes[0][chromosomeIndex][rightmostIndex]++;}
+			if(letterBeingAdded=='t'){votes[1][chromosomeIndex][rightmostIndex]++;}
+			if(letterBeingAdded=='g'){votes[2][chromosomeIndex][rightmostIndex]++;}
+			if(letterBeingAdded=='c'){votes[3][chromosomeIndex][rightmostIndex]++;}
+		}
+	}
+	totalGenomeLen = max;
+}
+
+void destroyVotes(int*** votes, int x, int y, int z){
+	for(int xi = 0; xi<x; xi++){
+		for(int yi = 0; yi<y; yi++){
+			delete[] votes[xi][yi];
+		}
+	}
+}
+
+float score(float temp, float MECval){
 	float out;
 	//Complete this!
 	return out;
@@ -99,10 +134,15 @@ std::vector<int> randomSolution(int k, std::vector<int> currentSolution){
 	return out;
 }
 
-std::vector<int> makeSolution(std::string genomeFileName, std::string readFileName, int in_numiters, float in_temp, float in_minTemp, float in_decreaseFactor){
-    std::vector<std::string> myGenome;
+std::vector<int> randomSolution(int k){
+	//To use when currentSolution is empty
+	std::vector<int> out;
+	return out;
+}
+
+
+std::vector<int> makeSolution(int k, int m, std::string readFileName, int in_numiters, float in_temp, float in_minTemp, float in_decreaseFactor){
     std::vector<read> myRead;
-	myGenome = readGenomeFile(genomeFileName);
     myRead = readReadFile(readFileName);
 	std::vector<int> minSolution;
 	std::vector<int> currentSolution;
@@ -110,14 +150,19 @@ std::vector<int> makeSolution(std::string genomeFileName, std::string readFileNa
 	float temp = in_temp;
 	float minTemp = in_minTemp;
 	float decreaseFactor = in_decreaseFactor;
+	int*** votes = new int[4][k][m];
+	currentSolution = randomSolution(k);
+	fillVotes(myRead, currentSolution, votes, m);
 	while(temp>minTemp){
 		for(int i = 0; i<numiters; i++){
-			if(MEC(currentSolution, myGenome, myRead) < MEC(minSolution, myGenome, myRead)){
+			int currentMEC = MEC(currentSolution, k, myRead, votes);
+			if( currentMEC < MEC(minSolution, k, myRead, votes)){
 				minSolution = currentSolution; //make sure vector assignment works like this in C!
 			}
-			std::vector<int> newSolution = randomSolution(myGenome.size(), currentSolution);
-			int scoreOfCurrent = score(currentSolution, myGenome, myRead);
-			int scoreOfNew = score(newSolution, myGenome, myRead);
+			std::vector<int> newSolution = randomSolution(k, currentSolution);
+			int newMEC = MEC(newSolution, k, myRead, votes);
+			int scoreOfCurrent = score(temp, newMEC);
+			int scoreOfNew = score(temp, newMEC);
 			float chanceToKeep = chanceToKeep(scoreOfCurrent, scoreOfNew, temp);
 			float randomIndex = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
 			if(chanceToKeep>randomIndex){
@@ -126,10 +171,9 @@ std::vector<int> makeSolution(std::string genomeFileName, std::string readFileNa
 		}
 		temp = temp * decreaseFactor;
 	}
+	destroyVotes(votes, 4, k, m);
 	return minSolution;
 }
-
-
 
 
 
@@ -142,5 +186,6 @@ int main()
     myRead = readReadFile("testr");
     return 0;
 }
+
 
 
